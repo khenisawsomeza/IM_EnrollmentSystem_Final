b2d3b857a6fd1f85c5c00cf1109258004236c86e
Khenyshi Hinlog <hinlogkhenyshi@gmail.com>
2025-07-31 01:06:13 +0800

    Add files via upload

---
+++ HinlogESystem/HinlogESystem/src/main/java/com/mycompany/hinlogesystem/Students.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hinlogesystem;
+
+/**
+ *
+ * @author kbhinlog
+ */
+public class Students extends HinlogESystem{
+    int studId;
+    String name, address, contact, gender, yearlvl;
+    
+    Students(){
+        connectDB();
+    }
+    
+    public void connectDB(){
+        DBConnect();
+        StudentForm form = new StudentForm();
+    }
+    
+    public void saveRecord(int id, String name, String address, String contact, String course, String gender, String yrLvl){
+        String query = "insert into 1stSem_Sy2025_2026.students values(" + id + ",'" + name + "','" + address + "','" + contact + "','" + course + "','" + gender + "','" + yrLvl + "');";
+        
+        try {
+            st.executeUpdate(query);
+            System.out.println("Insert Success");
+        } catch (Exception ex){
+            System.out.println("Failed to Insert: " + ex);
+        }
+    }
+    
+    public void deleteRecord(int id){
+        String query = "delete from 1stSem_Sy2025_2026.students where studId = " + id;
+        
+        try {
+            st.executeUpdate(query);
+            System.out.println("Delete Success");
+        } catch (Exception ex){
+            System.out.println("Failed to Delete: " + ex);
+        }
+    }
+    public void updateRecord(int id, String name, String address, String contact, String course, String gender, String yrLvl){
+        String query = "UPDATE 1stSem_Sy2025_2026.students SET " +
+               "studName = '" + name + "', " +
+               "studAddress = '" + address + "', " +
+               "studContact = '" + contact + "', " +
+               "studCourse = '" + course + "', " +
+               "studGender = '" + gender + "', " +
+               "studYrLvl = '" + yrLvl + "' " +
+               "WHERE studId = " + id;
+        
+        try {
+            st.executeUpdate(query);
+            System.out.println("Update Success");
+        } catch (Exception ex){
+            System.out.println("Failed to Update: " + ex);
+        }
+    }
+}
---
+++ HinlogESystem/HinlogESystem/src/main/java/com/mycompany/hinlogesystem/Teachers.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hinlogesystem;
+
+/**
+ *
+ * @author kbhinlog
+ */
+public class Teachers {
+    int tID;
+    String tName, tAdd, tContact, tEmail, tDept;
+    
+    public void saveRecord(){
+        System.out.println("Save Clicked");
+    }
+    public void deleteRecord(){
+        System.out.println("Delete Clicked");
+    }
+    public void updateRecord(){
+        System.out.println("Update Clicked");
+    }
+}
